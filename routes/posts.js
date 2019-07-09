b4deaa9679f291793daeecb8bbc6e62dd7bdfffb
Awa Melvine <melvineawa@gmail.com>
2019-07-09 14:53:53 +0100

    Validate get comments endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -25,11 +25,9 @@ router.post("/", async (req, res) => {
       res.status(201).json({ id });
     }
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        error: "There was an error while saving the post to the database"
-      });
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
   }
 });
 
@@ -67,10 +65,20 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id/comments", async (req, res) => {
   try {
     const { id } = req.params;
-    const comments = await Post.findPostComments(id);
-    res.status(200).json({ comments });
+    const post = await Post.findById(id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      const comments = await Post.findPostComments(id);
+      res.status(200).json({ comments });
+    }
   } catch (error) {
-    res.status(500).json({ error: "Could not get post comments" });
+    res
+      .status(500)
+      .json({ error: "The comments information could not be retrieved." });
   }
 });
 
