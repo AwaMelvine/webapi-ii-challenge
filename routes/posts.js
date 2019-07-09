d67007202035060383e390b87b2c16be14cfa510
Awa Melvine <melvineawa@gmail.com>
2019-07-09 12:33:04 +0100

    Implement the get posts endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
+const Post = require("../data/db");
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-  res.send({ message: "getting it all" });
+router.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.status(200).json({ posts });
 });
 
 module.exports = router;
